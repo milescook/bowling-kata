fd5393f1a0f8e76c66239052802155f2928b8493
Miles Cook <cook2909@gmaio.com>
2019-07-22 17:28:49 +0100

    New score from string function

--- spec/BowlingGameSpec.php
+++ spec/BowlingGameSpec.php
@@ -30,4 +30,23 @@ class BowlingGameSpec extends ObjectBehavior
         $this->beConstructedWith("1/1 X 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1");
         $this->getGameScore()->shouldReturn(30);
     }
+
+    function it_gets_scores_from_string()
+    {
+        $this->beConstructedWith("1/1 X 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1");
+        $expectedResult = 
+        [
+            1 => [1=>"1",2=>"1"],
+            2 => [1=>"X"],
+            3 => [1=>"1",2=>"1"],
+            4 => [1=>"1",2=>"1"],
+            5 => [1=>"1",2=>"1"],
+            6 => [1=>"1",2=>"1"],
+            7 => [1=>"1",2=>"1"],
+            8 => [1=>"1",2=>"1"],
+            9 => [1=>"1",2=>"1"],
+            10 => [1=>"1",2=>"1"]
+        ];
+        $this->getScoresAsArray()->shouldReturn($expectedResult);
+    }
 }
--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -13,10 +13,31 @@ class BowlingGame
         return $this->scoreString;
     }
 
+    public function getScoresAsArray()
+    {
+        $scoresArray = [];
+        $frame = 1;
+        
+        foreach(explode(" ",$this->scoreString) as $frameScore)
+        {
+            $throw = 1;
+            foreach(explode("/",$frameScore) as $throwScore)
+            {
+                $scoresArray[$frame][$throw] = $throwScore;
+                $throw++;
+            }
+            $frame++;
+        }
+
+        return $scoresArray;
+    }
+
     public function getGameScore()
     {
         $totalScore = 0;
         $lastRoundWasStrike = false;
+
+        
 
         foreach(explode(" ",$this->scoreString) as $frameScore)
         {
